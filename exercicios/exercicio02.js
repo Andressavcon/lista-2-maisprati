99a1f93892eb66c301507565867b3439bdb0d6d5
Andressavcon <andressavconrado@gmail.com>
2024-05-16 19:16:03 -0300

    feat: add exercicio02

--- exercicios/exercicio02.js
+++ exercicios/exercicio02.js
@@ -5,3 +5,21 @@ Caso ultrapasse 80 Km/h -1 , exiba uma mensagem dizendo que o usuário foi multa
 Nesse caso, exiba o valor da multa, cobrando R$ 5,00 por cada Km acima da velocidade permitida.
 
 */
+
+const prompt = require('prompt-sync')();
+const velocidade = prompt(`Qual velocidade do carro? (em Km/h-1)\n`);
+
+const verificarVelocidade = (velocidade) => {
+  if (velocidade > 80) {
+    const multa = (velocidade - 80) * 5;
+    return `
+    Usuário multado. Valor R$${multa.toFixed(2)}
+    `;
+  } else {
+    return `
+    Usuário sem multa.
+    `;
+  }
+};
+
+console.log(verificarVelocidade(velocidade));
