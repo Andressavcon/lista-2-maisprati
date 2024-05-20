73a51d896304d907c45df89219f9e490f96a175d
Andressavcon <andressavconrado@gmail.com>
2024-05-20 12:48:20 -0300

    feat: add exercicio06

--- exercicios/exercicio06.js
+++ exercicios/exercicio06.js
@@ -1,6 +1,27 @@
-/*
+const numeroSorteado = Math.floor(Math.random() * 5 + 1);
 
-Crie um jogo onde o computador vai sortear um número entre 1 e 5. O jogador vai
-tentar descobrir qual foi o valor sorteado.
+const prompt = require('prompt-sync')();
 
-*/
+let valorCerto = false;
+while (!valorCerto) {
+  const valorJogador = Number(prompt(`Adivinhe o número de 1 a 5: `));
+
+  if (valorJogador < 1 || valorJogador > 5) {
+    console.log(`
+    O número precisa ser entre 1 e 5
+    `);
+    continue;
+  }
+  if (isNaN(valorJogador)) {
+    console.log(`
+    O valor digitado precisa ser um número
+    `);
+    continue;
+  }
+  if (valorJogador === numeroSorteado) {
+    console.log(`
+    Parabéns você acertou!!!
+    `);
+    valorCerto = true;
+  }
+}
