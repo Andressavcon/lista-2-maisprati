463b4006a9c841b29cf2675516641d918d420334
Andressavcon <andressavconrado@gmail.com>
2024-05-20 15:06:48 -0300

    feat: add exercicio07

--- exercicios/exercicio07.js
+++ exercicios/exercicio07.js
@@ -1,17 +1,76 @@
-/*
+const prompt = require('prompt-sync')();
 
-Uma empresa de aluguel de carros precisa cobrar pelos seus serviços.
-O aluguel de um carro popular custa R$ 90,00 por dia e um carro de luxo custa R$ 150,00.
-Além disso, o cliente paga por Km percorrido. 
-Faça um programa que leia o tipo de carro alugado (popular ou luxo), quantos dias de aluguel e
-quantos Km foram percorridos. No final, mostre o preço a ser pago de acordo com os dados a seguir:
+function tipoDecarro() {
+  let validarTipo = true;
+  while (validarTipo) {
+    const carro = prompt(
+      `Qual o tipo de carro? (popular ou luxo) `
+    ).toLocaleLowerCase();
+    if (carro !== 'popular' && carro !== 'luxo') {
+      console.log(`
+        Insira um tipo exitente (popular ou luxo)
+        `);
+      continue;
+    } else {
+      return carro;
+    }
+  }
+}
 
-Carros populares
+function diasDeAluguel() {
+  let validarDias = true;
+  while (validarDias) {
+    let dias = parseInt(prompt(`Quantos dias? (valor em dias) `));
+    if (dias < 1) {
+      console.log(`
+        O número de dias precisa ser maior que 0
+        `);
+      continue;
+    } else {
+      return dias;
+    }
+  }
+}
 
-- Até 100 Km percorridos: R$ 0,20 por Km
-- Acima de 100 Km percorridos: R$ 0,10 por Km
-  Carros de luxo
-- Até 200 Km percorridos: R$ 0,30 por Km
-- Acima de 200 Km percorridos: R$ 0,25 por Km
+function kmPercorridos() {
+  let validarKm = true;
+  while (validarKm) {
+    let km = parseInt(prompt(`Quantos Km? (valor em km) `));
+    if (km < 1) {
+      console.log(`
+              O número de Km precisa ser maior que 0
+              `);
+      continue;
+    } else {
+      return km;
+    }
+  }
+}
 
-*/
+function calcularServicos() {
+  const carro = tipoDecarro();
+  const dias = diasDeAluguel();
+  const km = kmPercorridos();
+
+  let resultado = 0;
+  if (carro === 'popular') {
+    if (km <= 100) {
+      resultado = (0.2 * km + dias * 90).toFixed(2);
+    } else {
+      resultado = (0.1 * km + dias * 90).toFixed(2);
+    }
+  } else {
+    if (km <= 200) {
+      resultado = (0.3 * km + dias * 150).toFixed(2);
+    } else {
+      resultado = (0.25 * km + dias * 150).toFixed(2);
+    }
+  }
+
+  return `
+  Carro ${carro}, alugado por ${dias} dias, percorrendo ${km}Km
+  =====> Total R$${resultado}
+  `;
+}
+
+console.log(calcularServicos());
