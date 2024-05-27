a6e9066e568b8d792a4c5b5ce532c2d6fdc2da3b
Andressavcon <andressavconrado@gmail.com>
2024-05-27 11:19:20 -0300

    feat: add exercicio 41

---
+++ exercicios/exercicio41.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: 'andressa',
+  idade: 29,
+};
+
+console.log(pessoa);
+console.log(`A idade é de ${pessoa.idade}`);
+
+pessoa.email = 'andressa@email.com';
+console.log(pessoa);
