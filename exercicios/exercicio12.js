bfd4154e61461eab3d499742c7876dea99b088f4
Andressavcon <andressavconrado@gmail.com>
2024-06-02 14:17:47 -0300

    feat: add exercicio 12 e 13

--- exercicios/exercicio12.js
+++ exercicios/exercicio12.js
@@ -1,7 +1,9 @@
-/*
+// F(n)=F(n−1)+F(n−2)
 
-Faça um programa que mostre os 10 primeiros elementos
- da Sequência de Fibonacci.
-Ex.: 1, 1, 2, 3, 5, 8, 13, 21.
+let fibonacci = [1, 1];
 
-*/
+for (let i = 2; i < 10; i++) {
+  fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+console.log(`${fibonacci}`);
--- exercicios/exercicio13.js
+++ exercicios/exercicio13.js
@@ -1,8 +1,11 @@
-/*
+// F(n)=F(n−1)+F(n−2)
 
-Crie um programa que preencha automaticamente 
-(usando lógica, não apenas atribuindo diretamente) 
-um vetor numérico com 15 posições com os primeiros 
-elementos da sequência de Fibonacci.
+let fibonacci = [];
+fibonacci[0] = 0;
+fibonacci[1] = 1;
 
-*/
+for (let i = 2; i < 15; i++) {
+  fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+console.log(fibonacci);
