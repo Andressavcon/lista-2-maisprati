871aa0d530e252096760cc60c2f0617d563a1e35
Andressavcon <andressavconrado@gmail.com>
2024-06-06 23:31:13 -0300

    feat: add exercicio 30

--- exercicios/exercicio30.js
+++ exercicios/exercicio30.js
@@ -1,7 +1,50 @@
-/*
+const prompt = require('prompt-sync')();
 
-Escrever um algoritmo que lê uma matriz M(5,5) e cria 2 vetores SL(5) 
-e SC(5) que contenham, respectivamente, as somas das linhas e das colunas de M.
-Escrever a matriz e os vetores criados.
+const tamanhoMatriz = 3;
 
-*/
+let M = [];
+let SL = [];
+let SC = [];
+
+for (let i = 0; i < tamanhoMatriz; i++) {
+  SL.push(0);
+  SC.push(0);
+}
+
+for (let i = 0; i < tamanhoMatriz; i++) {
+  M.push([]);
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    let valor = lerNumeroInteiro(
+      'Digite o elemento [' + (i + 1) + '][' + (j + 1) + '] da matriz M: '
+    );
+    M[i].push(valor);
+
+    SL[i] += valor;
+
+    SC[j] += valor;
+  }
+}
+
+function validarEntrada(entrada) {
+  return !isNaN(entrada) && entrada.trim() !== '';
+}
+
+function lerNumeroInteiro(mensagem) {
+  let entrada = prompt(mensagem);
+  while (!validarEntrada(entrada)) {
+    entrada = prompt('Por favor, digite um número inteiro válido: ');
+  }
+  return parseInt(entrada);
+}
+
+console.log(`
+Matriz M:`);
+for (let i = 0; i < tamanhoMatriz; i++) {
+  console.log(M[i].join(' '));
+}
+
+console.log(`
+Vetor SL(Soma Linhas): ${SL.join(' ')}`);
+
+console.log(`
+Vetor SC(Soma Colunas): ${SC.join(' ')}`);
