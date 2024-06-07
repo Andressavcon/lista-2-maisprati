965fc1ea7f61bf2c28dc9181b2807bc2e946ed56
Andressavcon <andressavconrado@gmail.com>
2024-06-07 15:12:16 -0300

    feat: add exercicio 33

--- exercicios/exercicio33.js
+++ exercicios/exercicio33.js
@@ -1,7 +1,57 @@
-/*
+const prompt = require('prompt-sync')();
 
-Faça um algoritmo que leia uma matriz 3 x 3 e após a leitura,
-multiplique os elementos da diagonal principal com a média dos
-elementos da diagonal secundária.
+const tamanhoMatriz = 3;
 
-*/
+function validarEntrada(entrada) {
+  return !isNaN(entrada) && entrada.trim() !== '';
+}
+
+function lerNumeroInteiro(mensagem) {
+  let entrada = prompt(mensagem);
+  while (!validarEntrada(entrada)) {
+    entrada = prompt('Por favor, digite um número inteiro válido: ');
+  }
+  return parseInt(entrada);
+}
+
+let M = [];
+
+for (let i = 0; i < tamanhoMatriz; i++) {
+  M.push([]);
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    let valor = lerNumeroInteiro(
+      'Digite o elemento [' + (i + 1) + '][' + (j + 1) + '] da matriz M: '
+    );
+    M[i].push(valor);
+  }
+}
+
+let somaDiagonalSecundaria = 0;
+for (let i = 0; i < tamanhoMatriz; i++) {
+  somaDiagonalSecundaria += M[i][tamanhoMatriz - 1 - i];
+}
+let mediaDiagonalSecundaria = somaDiagonalSecundaria / tamanhoMatriz;
+
+let matrizModificada = [];
+for (let i = 0; i < tamanhoMatriz; i++) {
+  matrizModificada.push([]);
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    matrizModificada[i].push(M[i][j]);
+  }
+}
+
+for (let i = 0; i < tamanhoMatriz; i++) {
+  matrizModificada[i][i] = (M[i][i] * mediaDiagonalSecundaria).toFixed(1);
+}
+
+console.log(`
+Matriz M original:`);
+for (let i = 0; i < tamanhoMatriz; i++) {
+  console.log(M[i].join(' '));
+}
+
+console.log(`
+Matriz Modificada:`);
+for (let i = 0; i < tamanhoMatriz; i++) {
+  console.log(matrizModificada[i].join(' '));
+}
