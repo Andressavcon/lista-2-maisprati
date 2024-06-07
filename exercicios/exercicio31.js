65625e2a91339c6c23b73e5edea38a7a0e0bfe56
Andressavcon <andressavconrado@gmail.com>
2024-06-06 22:46:46 -0300

    feat: add exercicio 31

--- exercicios/exercicio31.js
+++ exercicios/exercicio31.js
@@ -1,8 +1,63 @@
-/*
+const prompt = require('prompt-sync')();
 
-Escreva um algoritmo que leia um número inteiro A e uma matriz V 30 x 30 de inteiros.
-Conte quantos valores iguais a A estão na matriz. 
-Crie, a seguir, uma matriz X contendo todos os elementos de V diferentes de A. 
-Mostre os resultados.
+let tamanhoMatriz = 30;
 
-*/
+let A = lerNumeroInteiro('Digite um número inteiro A: ');
+
+let V = [];
+for (let i = 0; i < tamanhoMatriz; i++) {
+  V[i] = [];
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    let valor = lerNumeroInteiro(`Digite o elemento [${i + 1}][${j + 1}]: `);
+    V[i][j] = valor;
+  }
+}
+
+let X = [];
+for (let i = 0; i < tamanhoMatriz; i++) {
+  X[i] = [];
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    X[i][j] = V[i][j];
+  }
+}
+
+let countA = 0;
+for (let i = 0; i < tamanhoMatriz; i++) {
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    if (V[i][j] === A) {
+      countA++;
+      X[i][j] = 'X';
+    }
+  }
+}
+
+for (let i = 0; i < tamanhoMatriz; i++) {
+  let novaLinha = [];
+  for (let j = 0; j < tamanhoMatriz; j++) {
+    if (X[i][j] !== undefined) {
+      novaLinha.push(X[i][j]);
+    }
+  }
+  X[i] = novaLinha;
+}
+
+function validarEntrada(entrada) {
+  return !isNaN(entrada) && entrada.trim() !== '';
+}
+
+function lerNumeroInteiro(mensagem) {
+  let entrada = prompt(mensagem);
+  while (!validarEntrada(entrada)) {
+    entrada = prompt('Por favor, digite um número inteiro válido: ');
+  }
+  return parseInt(entrada);
+}
+
+console.log(`
+Quantidade de valores iguais a A na matriz V: ${countA}`);
+
+console.log(`
+Matriz X com os elementos de V diferentes de A:`);
+for (const linha of X) {
+  console.log(linha.join(' '));
+}
